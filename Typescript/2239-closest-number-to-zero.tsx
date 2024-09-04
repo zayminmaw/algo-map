4e34f89aff5f3c415c5886c462b5026a91b4b431
zayminmaw <zayminmaw77@gmail.com>
2024-09-04 09:15:37 +0700

    feat: added explanation and question link

--- Typescript/2239-closest-number-to-zero.tsx
+++ Typescript/2239-closest-number-to-zero.tsx
@@ -1,3 +1,6 @@
+// Explanation https://www.youtube.com/watch?v=dLlKA5DQKYs
+// Question https://leetcode.com/problems/find-closest-number-to-zero/description/
+
 function findClosestNumber(nums: number[]): number {
   let closestNum = nums[0];
   nums.forEach((num) => {
